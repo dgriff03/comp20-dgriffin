9238c123c3b7065f32ba82c59f60e224410957aa
Daniel <daniel.griffin@tufts.edu>
2013-02-09 16:16:35 -0500

    first commit

---
+++ frogger/game.js
@@ -0,0 +1,18 @@
+// Your work goes here...
+//blue = #191970
+//black = #000000
+//header "Frogger"
+//purple roadside
+//lives
+// level number
+//score
+//high score
+//fron facing up
+//two vehicles
+//log in water
+
+
+function start_game(){
+
+
+}
