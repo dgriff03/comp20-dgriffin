ac0566f19a4d6762c5c790d50c021778fcff2446
Daniel <daniel.griffin@tufts.edu>
2013-02-09 17:02:45 -0500

    wrote variable names in javascript file

--- frogger/game.js
+++ frogger/game.js
@@ -11,8 +11,25 @@
 //two vehicles
 //log in water
 
+var cord-x,cord-y,lives,game_over,level;
+var speed_car1,speed_car2,speed_log;
+var x_log1,y_log1; 
+var x_car1,y_car1;
+var x_car2,y_car2;
+var time_allowed,time,cur_time;
 
 function start_game(){
+	initialize();
+	draw_board();
+}
 
+function draw_board(){
+	cur_time = new Date(milliseconds);
+	
 
+}
+
+function initialize(){
+	speed_car1 = speed_car2 = speed_log = 1;
+	
 }
