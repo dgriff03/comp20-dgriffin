6eef41af75ce27fabcb9e43394cce29294e43257
Daniel <daniel.griffin@tufts.edu>
2013-02-14 09:39:15 -0500

    added water and landing pads

--- frogger/game.js
+++ frogger/game.js
@@ -11,12 +11,6 @@
 //two vehicles
 //log in water
 
-var cord-x,cord-y,lives,game_over,level;
-var speed_car1,speed_car2,speed_log;
-var x_log1,y_log1; 
-var x_car1,y_car1;
-var x_car2,y_car2;
-var time_allowed,time,cur_time;
 
 function start_game(){
 	initialize();
@@ -24,12 +18,24 @@ function start_game(){
 }
 
 function draw_board(){
-	cur_time = new Date(milliseconds);
-	
+		canvas = document.getElementById('game');
+		if(canvas.getContext){
+			ctx = canvas.getContext('2d');
+			ctx.save();
+			img = new Image();
+			img.src = 'assets/frogger_sprites.png';
+			ctx.fillStyle = "rgb(19, 19, 70)";
+			ctx.fillRect (0,0, 399, 370);
+			ctx.drawImage(img,0,55,399,55,0,55,399,55);
 
+			
+		}
+		else{
+			alert('Sorry, canvas not supported by your browser');
+		}
 }
 
 function initialize(){
 	speed_car1 = speed_car2 = speed_log = 1;
-	
+	cur_time = new Date();
 }
