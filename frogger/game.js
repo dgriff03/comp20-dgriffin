b496884bd63039e61f1d30e71b96ce6ed4ad593e
Daniel <daniel.griffin@tufts.edu>
2013-02-14 09:59:33 -0500

    added comments

--- frogger/game.js
+++ frogger/game.js
@@ -15,7 +15,7 @@
 function start_game(){
 	initialize();
 	draw_board();
-}
+}s
 
 function draw_board(){
 		canvas = document.getElementById('game');
@@ -25,15 +25,15 @@ function draw_board(){
 			img = new Image();
 			img.src = 'assets/frogger_sprites.png';
 			ctx.fillStyle = "rgb(19, 19, 70)";
-			ctx.fillRect (0,0, 399, 275);
-			ctx.drawImage(img,10,10,325,40,15,15,325,40);
-			ctx.drawImage(img,0,55,399,55,0,55,399,55);
-			ctx.drawImage(img,5,165,180,25,20,185,180,25);
-			ctx.drawImage(img,5,195,120,25,90,240,120,25);
-			ctx.drawImage(img,0,119,399,35,0,275,399,35);
-			ctx.drawImage(img,0,119,399,35,0,475,399,35);
+			ctx.fillRect (0,0, 399, 275);//water
+			ctx.drawImage(img,10,10,325,40,15,15,325,40);	//frogger
+			ctx.drawImage(img,0,55,399,55,0,55,399,55);		//pads
+			ctx.drawImage(img,5,165,180,25,20,185,180,25);	//log 1
+			ctx.drawImage(img,5,195,120,25,90,240,120,25); //log 2
+			ctx.drawImage(img,0,119,399,35,0,275,399,35); //side road top
+			ctx.drawImage(img,0,119,399,35,0,475,399,35);//side road bottom
 			ctx.fillStyle = "rgb(00, 00, 00)";
-			ctx.fillRect (0,309,399,166);
+			ctx.fillRect (0,309,399,166);//road
 
 		}
 		else{
