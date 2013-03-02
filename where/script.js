45705b696f2fedf563afce49ea072bb97257818e
Daniel <daniel.griffin@tufts.edu>
2013-03-02 17:12:16 -0500

    based code off of gmap_api_example. Added parsing

---
+++ where/script.js
@@ -0,0 +1,86 @@
+	var request;
+	var
+
+	function init()
+			{
+				// Faneuil Hall
+				var landmark = new google.maps.LatLng(42.3599611, -71.0567528);
+
+				// Set up map
+				var myOptions = {
+					zoom: 13, // The larger the zoom number, the bigger the zoom
+					center: landmark,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+
+				// Create the map in the "map_canvas" <div>
+				var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+				// Create a marker				
+				var marker = new google.maps.Marker({
+					position: landmark,
+					title: "Faneuil Hall, Boston, MA"
+				});
+				marker.setMap(map);
+
+				// This is a global info window...
+				var infowindow = new google.maps.InfoWindow();
+
+				// Open info window on click of marker
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(map, marker);
+				});
+			}
+			
+	function get_trains(){
+		request = new XMLHttpRequest();
+		request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", true);
+		request.send(null);
+		request.onreadystatechange = callback(true);
+	}
+		
+	function callback(trains) {
+        if (request.readyState == 4 && request.status == 200) {
+			if(trains){
+				parse_trains();
+			}
+			if(!trains){
+				parse_stops();
+			}
+        }
+    }
+	
+	function parse_trains(){
+		var str = request.responseText;
+		trains = JSON.parse(str);
+		num_trains = trains.length;
+		add_trains();
+	}
+	
+	function add_trains(){
+	
+	}
+
+	function get_stops(){
+		request_stops = new XMLHttpRequest();
+		request_stops.open("GET","/red_stops.json",false);
+		request_stops.send(null);
+		request_stops.onreadystatechange = callback(false);
+	}
+	
+	function parse_stops(){
+		var str = request_stops.responseText;
+		stop_info = JSON.parse(str);
+		num_stops = stop_info.length;
+		add_stops();
+	}
+	
+	function add_stops(){
+		stops = new Array();
+		for(int i = 0; i < num_stops;i++){
+			
+	
+	
+		}
+	}
